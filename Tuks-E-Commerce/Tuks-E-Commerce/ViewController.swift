34bd1d33167e5f9371f332c4217e5017d82dafd5
narrminn <nerminalesova@gmail.com>
2026-02-04 18:24:46 +0400

    initial setup

---
+++ Tuks-E-Commerce/Tuks-E-Commerce/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Tuks-E-Commerce
+//
+//  Created by Narmin Alasova on 04.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
